d27a79c5de52acf118b5390bdfca27f76cee425f
Diego Morales <dmorales-2022171@kinal.edu.gt>
2023-06-05 15:30:51 -0600

    Subiendo Backend

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = Schema({
+    nombre:{
+        type: String,
+        require:[true,'The name is require']
+    },
+
+    userName:{
+        type: String,
+        require:[true , 'The nickname is require']
+    },
+    password:{
+        type:String,
+        require:[true, 'password is require']
+    },
+    
+    NoCuenta:{
+        type:String,
+        require:[true,'The NoCuenta is require']
+    },
+    Dpi:{
+        type: Number,
+        requiere:[true,'Dpi is Require']
+    },
+    Celular:{
+        type: Number,
+        require:[true, 'Celular is require']
+    },
+    email:{
+        type: String,
+        require:[true,'Email is require']
+    },
+
+    NamefromWork:{
+        type: String,
+        
+    },
+    IngresosMensauales:{
+        type: Number,
+        require:[true, "Los ingresos son requeridos"]
+    },
+    role:{
+        type: String,
+        default : "User_rol"
+    },
+    estado:{
+        type: Boolean,
+        default: true
+    },
+
+})
+
+module.exports = model('User', userSchema)
