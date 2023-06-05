4fc96856e63067a14fd401d05f0bf9f2d9c4b2bf
Joshua Alvarez <jalvarez-2021317@kinal.edu.gt>
2023-06-05 16:11:55 -0600

    Update

--- controllers/user.js
+++ controllers/user.js
@@ -2,51 +2,57 @@ const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 
-
 const createAdminUser = async () => {
-    const adminUser = await User.findOne({ role: "Administrador" });
-    if (!adminUser) {
-      const salt = bcryptjs.genSaltSync();
-      const userData = {
-        nombre: "ADMINB",
-        userName: "ADMINB",
-        password: bcryptjs.hashSync("ADMINB", salt),
-        NoCuenta: "123456789",
-        Dpi: 1234567890123,
-        Celular: 1234567890,
-        email: "admin@example.com",
-        IngresosMensauales: 5000,
-        role: "Administrador"
-      };
-  
-      const userDB = new User(userData);
-      await userDB.save();
-      console.log('Usuario administrador creado automáticamente');
-    } else {
-      console.log('Ya existe un usuario administrador en la base de datos');
-    }
-  };
-  
-  // Llama a la función para crear el usuario administrador al iniciar el proyecto
-  createAdminUser();
+  const adminUser = await User.findOne({ role: "Administrador" });
+  if (!adminUser) {
+    const salt = bcryptjs.genSaltSync();
+    const userData = {
+      nombre: "ADMINB",
+      userName: "ADMINB",
+      password: bcryptjs.hashSync("ADMINB", salt),
+      NoCuenta: "123456789",
+      Dpi: 1234567890123,
+      Celular: 1234567890,
+      email: "admin@example.com",
+      IngresosMensauales: 5000,
+      role: "Administrador"
+    };
 
-const getUsers = async ({query}, res) => {
-    const listaUsers = await Promise.all([
-        User.countDocuments(query),
-        User.find(query)
-    ]);
-    res.json({msg: 'GET API de Users', listaUsers});
-}
+    const userDB = new User(userData);
+    await userDB.save();
+    console.log('Usuario administrador creado automáticamente');
+  } else {
+    console.log('Ya existe un usuario administrador en la base de datos');
+  }
+};
+
+// Llama a la función para crear el usuario administrador al iniciar el proyecto
+createAdminUser();
+
+const getUsers = async ({ query }, res) => {
+  const listaUsers = await Promise.all([
+    User.countDocuments(query),
+    User.find(query)
+  ]);
+  res.json({ msg: 'GET API de Users', listaUsers });
+};
 
 const postUser = async ({ body }, res) => {
   const salt = bcryptjs.genSaltSync();
-  const randomNoCuenta = Math.floor(Math.random() * 5000) + 1;
+  let randomNoCuenta = Math.floor(Math.random() * 5000) + 1;
 
-  // Verificar si ya existe un usuario con el mismo NoCuenta
-  const existingUser = await User.findOne({ NoCuenta: randomNoCuenta });
+  let existingUser = await User.findOne({ NoCuenta: randomNoCuenta });
+  let attempts = 0;
+  const maxAttempts = 10;
+
+  while (existingUser && attempts < maxAttempts) {
+    randomNoCuenta = Math.floor(Math.random() * 5000) + 1;
+    existingUser = await User.findOne({ NoCuenta: randomNoCuenta });
+    attempts++;
+  }
+
   if (existingUser) {
-    // Si existe, generar otro número de cuenta único
-    return postUser({ body }, res);
+    return res.status(400).json({ error: 'No se pudo generar un número de cuenta único' });
   }
 
   const userData = {
@@ -61,21 +67,32 @@ const postUser = async ({ body }, res) => {
 };
 
 
-const putUser = async ({params, body}, res) => {
-    const salt = bcryptjs.genSaltSync();
-    const UserEditado = await User.findByIdAndUpdate(params.id, {...body, password: bcryptjs.hashSync(body.password, salt)});
-    res.json({msg: 'PUT API de User', UserEditado});
-}
+const putUser = async ({ params, body }, res) => {
+  const allowedFields = ['nombre', 'userName', 'password', 'email'];
+  const updates = {};
 
-const deleteUser = async ({params}, res) => {
-    const UserEliminado = await User.findByIdAndDelete(params.id);
-    res.json({msg: 'DELETE API de User', UserEliminado});
-}
+  for (const field of allowedFields) {
+    if (body[field]) {
+      updates[field] = body[field];
+    }
+  }
+
+  const salt = bcryptjs.genSaltSync();
+  updates.password = bcryptjs.hashSync(body.password, salt);
+
+  const UserEditado = await User.findByIdAndUpdate(params.id, updates);
+  res.json({ msg: 'PUT API de User', UserEditado });
+};
+
+const deleteUser = async ({ params }, res) => {
+  const UserEliminado = await User.findByIdAndDelete(params.id);
+  res.json({ msg: 'DELETE API de User', UserEliminado });
+};
 
 module.exports = {
-    getUsers,
-    postUser,
-    putUser,
-    deleteUser,
-    createAdminUser
-}
+  getUsers,
+  postUser,
+  putUser,
+  deleteUser,
+  createAdminUser
+};
