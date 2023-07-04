cd7303489af6ee5dd09c8fac327f2c9aadf1d241
Joshua Alvarez <jalvarez-2021317@kinal.edu.gt>
2023-07-03 19:38:00 -0600

    Transacciones Update

--- controllers/Transaccion.js
+++ controllers/Transaccion.js
@@ -1,21 +1,45 @@
 const { response, request } = require('express');
-const  Transaccion = require('../models/transacciones');
+const Transaccion = require('../models/transacciones');
 const Cuenta = require('../models/cuentas');
 
+const getTransaccionesPorCuenta = async (req, res) => {
+  const cuentaId = req.params.cuentaId;
+  try {
+    // Obtener todas las transacciones relacionadas con la cuenta específica
+    const transacciones = await Transaccion.find({
+      $or: [
+        { cuentaOrigen: cuentaId },
+        { cuentaDestino: cuentaId }
+      ]
+    });
+    
+    res.json(transacciones);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener las transacciones de la cuenta' });
+  }
+};
 
 const getTransacciones = async (req, res) => {
-  const transacciones = await Transaccion.find();
-  res.json(transacciones);
+  try {
+    const transacciones = await Transaccion.find();
+    res.json(transacciones);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener las transacciones' });
+  }
 };
 
 const postTransaccion = async (req, res) => {
-
   const newTransaccion = new Transaccion({
     cuentaOrigen: req.body.cuentaOrigen,
     cuentaDestino: req.body.cuentaDestino,
     monto: req.body.monto,
     fecha: req.body.fecha
   });
+
+  // Check if the amount exceeds the limit
+  if (req.body.monto > 10000) {
+    return res.status(400).json({ error: 'Las transferencias deben ser menores a 10,000' });
+  }
 
   // Check if the source account has enough balance
   const cuentaOrigenInfo = await Cuenta.findById(req.body.cuentaOrigen);
@@ -38,7 +62,6 @@ const postTransaccion = async (req, res) => {
 };
 
 
-
 const deleteTransaccion = async (req, res) => {
   const transaccion = await Transaccion.findByIdAndRemove(req.params.id);
   if (!transaccion) {
@@ -49,6 +72,7 @@ const deleteTransaccion = async (req, res) => {
 };
 
 module.exports = {
+  getTransaccionesPorCuenta,
   getTransacciones,
   postTransaccion,
   deleteTransaccion
--- routes/transacciones.js
+++ routes/transacciones.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get('/ver', getTransacciones);
 router.post('/New', postTransaccion);
 router.delete('/transacciones/:id', deleteTransaccion);
+// Ruta para obtener todas las transacciones de una cuenta específica
+router.get('/cuentas/:cuentaId/transacciones', transaccionController.getTransaccionesPorCuenta);
 
 module.exports = router;
